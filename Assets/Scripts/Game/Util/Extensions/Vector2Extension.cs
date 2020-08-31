e61193923ce4830275ba2461b5189a996a96ff2e
HULKSTD <hulk1257@gmail.com>
2020-08-31 16:20:02 +0900

    Add Tuple to Vector Extension

--- Assets/Scripts/Game/Util/Extensions/Vector2Extension.cs
+++ Assets/Scripts/Game/Util/Extensions/Vector2Extension.cs
@@ -8,6 +8,16 @@ namespace Game.Util.Extensions
         {
             return (vector2.x, vector2.y);
         }
+        
+        public static Vector3 ToVector3FromVector2(this (float x, float y) vector2Tuple)
+        {
+            return new Vector3(vector2Tuple.x, vector2Tuple.y);
+        }
+        
+        public static Vector2 ToVector2(this (float x, float y) vector2Tuple)
+        {
+            return new Vector2(vector2Tuple.x, vector2Tuple.y);
+        }
 
         public static Vector3 ToVector3(this Vector2 vector2, float z = 0)
         {
--- Assets/Scripts/Game/Util/Extensions/Vector3Extension.cs
+++ Assets/Scripts/Game/Util/Extensions/Vector3Extension.cs
@@ -9,9 +9,14 @@ namespace Game.Util.Extensions
             return (vector3.x, vector3.y, vector3.y);
         }
 
-        public static Vector2 ToVector2(this (float x, float y, float z) vector3Tuple)
+        public static Vector3 ToVector3(this (float x, float y, float z) vector3Tuple)
         {
-            return new Vector2(vector3Tuple.x, vector3Tuple.y);
+            return new Vector3(vector3Tuple.x, vector3Tuple.y, vector3Tuple.z);
+        }
+        
+        public static Vector2 ToVector2FromVector3(this (float x, float y, float z) vector2Tuple)
+        {
+            return new Vector2(vector2Tuple.x, vector2Tuple.y);
         }
 
         public static Vector2 ToVector2(this Vector3 vector3)
