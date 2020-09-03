6029e1a4ab709f4bdf90e4af3ec5f6473d7005e7
HULKSTD <hulk1257@gmail.com>
2020-09-03 11:17:47 +0900

    Add RectTransform in ScrollerContent

--- Assets/Scripts/Game/UI/Scroller/Core/ScrollContent.cs
+++ Assets/Scripts/Game/UI/Scroller/Core/ScrollContent.cs
@@ -17,7 +17,13 @@ namespace Game.UI.Scroller.Core
     public class ScrollContent<TData> : MonoBehaviour where TData : ScrollContent.ScrollContentData
     {
         private TData _data;
+        public RectTransform RectTransform { get; private set; }
 
+        protected virtual void Awake()
+        {
+            RectTransform = (RectTransform)transform;
+        }
+        
         public virtual void SetContentData(TData tdata)
         {
             _data = tdata;
