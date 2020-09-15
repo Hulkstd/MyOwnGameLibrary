666953a31aa7923147b978e4715379c019038b5f
HULKSTD <hulk1257@gmail.com>
2020-09-15 09:16:15 +0900

    Seperate Tweener Function.

---
+++ Assets/Scripts/Game/Tweener/Core/Tween.cs
@@ -0,0 +1,62 @@
+﻿using System.Diagnostics;
+using Game.Threading;
+using Game.Util;
+
+namespace Game.Tweener.Core
+{
+    public class Tween
+    {
+        protected int ThreadKey;
+        protected Stopwatch Stopwatch;
+        
+        protected Utility.Curves.Ease Ease;
+        protected bool From;
+        protected bool Loop;
+        protected LoopType LoopType;
+        protected float Duration;
+        
+        public bool IsPlaying { get; protected set; }
+        public bool IsPausing { get; protected set; }
+        public bool IsComplete { get; protected set; }
+        
+        public TweenerCallback OnUpdate;
+        public TweenerCallback OnComplete;
+        
+        public virtual void Play()
+        {
+            if (IsPlaying)
+                return;
+
+            IsPlaying = true;
+            IsPausing = false;
+            IsComplete = false;
+            Stopwatch.Reset();
+            Stopwatch.Start();
+        }
+
+        public virtual void Stop()
+        {
+            if(MonoMultiThread.IsContainPauseList(ThreadKey))
+                MonoMultiThread.ResumeThreadWorker(ThreadKey);
+            if(MonoMultiThread.IsContainThreadWorker(ThreadKey))
+                MonoMultiThread.DeleteThreadWorker(ThreadKey);
+
+            IsPlaying = false;
+            IsComplete = false;
+            IsPausing = false;
+        }
+
+        public virtual void Restart()
+        {
+            Play();
+        }
+
+        public virtual void Pause()
+        {
+            MonoMultiThread.PauseThreadWorker(ThreadKey);
+            IsPlaying = false;
+            IsPausing = true;
+            Stopwatch.Stop();
+        }
+    }
+}
--- Assets/Scripts/Game/Tweener/Core/Tweener.cs
+++ Assets/Scripts/Game/Tweener/Core/Tweener.cs
@@ -11,30 +11,16 @@ namespace Game.Tweener.Core
     /*todo
      Play(), Stop(), Restart(), Pause()
      */
-    public class Tweener<T, TTweenData> : Utility.IPoolingData
+    public class Tweener<T, TTweenData> : Tween, Utility.IPoolingData
         where TTweenData : ITweenData<T>
     {
-        private int _threadKey;
-        private Stopwatch _stopwatch;
         private TweenerSetter<T> _setter;
         private TweenerGetter<T> _getter;
         private TTweenData _tweenFunc;
 
-        private Utility.Curves.Ease _ease;
-        private bool _from;
-        private bool _loop;
-        private LoopType _loopType;
-        private float _duration;
-
         private T _startValue;
         private T _endValue;
         private float _durationValue;
-
-        public bool IsPlaying { get; private set; }
-        public bool IsPausing { get; private set; }
-        public bool IsComplete { get; private set; }
-        public TweenerCallback OnUpdate;
-        public TweenerCallback OnComplete;
 
         public static Tweener<T, TTweenData> To(TweenerSetter<T> setter, TweenerGetter<T> getter,
             TTweenData tweenData, T endValue, float duration)
@@ -47,102 +33,78 @@ namespace Game.Tweener.Core
         private void Initialize(TweenerSetter<T> setter, TweenerGetter<T> getter, TTweenData tweenData, T endValue,
             float duration)
         {
-            _threadKey = -999;
-            _stopwatch = Stopwatch.StartNew();
+            ThreadKey = -999;
+            Stopwatch = Stopwatch.StartNew();
             
             IsPlaying = false;
             IsPausing = false;
             IsComplete = true;
-            _ease = Utility.Curves.Ease.InSine;
-            _from = false;
+            Ease = Utility.Curves.Ease.InSine;
+            base.From = false;
 
             _setter = setter;
             _getter = getter;
             _tweenFunc = tweenData;
             _startValue = getter();
             _endValue = endValue;
-            _duration = duration;
+            Duration = duration;
             _durationValue = 0f;
         }
 
         public Tweener<T, TTweenData> SetEase(Utility.Curves.Ease ease)
         {
-            _ease = ease;
+            Ease = ease;
             return this;
         }
 
         public Tweener<T, TTweenData> SetDuration(float duration)
         {
-            _duration = duration;
+            Duration = duration;
             return this;
         }
 
         public Tweener<T, TTweenData> From(T from)
         {
-            _from = true;
+            base.From = true;
             _startValue = from;
             return this;
         }
 
         public Tweener<T, TTweenData> SetLoop(bool flag, LoopType loopType = LoopType.Normal)
         {
-            _loop = flag;
-            _loopType = loopType;
+            Loop = flag;
+            LoopType = loopType;
             return this;
         }
 
-        public void Play()
+        public override void Play()
         {
+            base.Play();
             if (IsPlaying)
                 return;
-
-            IsPlaying = true;
-            IsPausing = false;
-            IsComplete = false;
-            _stopwatch.Reset();
-            _stopwatch.Start();
-            if (_threadKey != -999)
+            
+            if (ThreadKey != -999)
             {
-                if (MonoMultiThread.IsContainThreadWorker(_threadKey) && MonoMultiThread.IsContainPauseList(_threadKey))
+                if (MonoMultiThread.IsContainThreadWorker(ThreadKey) && MonoMultiThread.IsContainPauseList(ThreadKey))
                 {
-                    MonoMultiThread.ResumeThreadWorker(_threadKey);
-                    _stopwatch.Start();
+                    MonoMultiThread.ResumeThreadWorker(ThreadKey);
+                    Stopwatch.Start();
                 }
                 else
                 {
-                    _threadKey = MonoMultiThread.InsertThreadWorker(WorkThreadAction, MainThreadAction, _startValue, _threadKey);
+                    ThreadKey = MonoMultiThread.InsertThreadWorker(WorkThreadAction, MainThreadAction, _startValue, ThreadKey);
                 }
             }
             else
             {
-                _threadKey = MonoMultiThread.InsertThreadWorker(WorkThreadAction, MainThreadAction, _startValue);
+                ThreadKey = MonoMultiThread.InsertThreadWorker(WorkThreadAction, MainThreadAction, _startValue);
             }
         }
 
-        public void Stop()
-        {
-            if(MonoMultiThread.IsContainPauseList(_threadKey))
-                MonoMultiThread.ResumeThreadWorker(_threadKey);
-            if(MonoMultiThread.IsContainThreadWorker(_threadKey))
-                MonoMultiThread.DeleteThreadWorker(_threadKey);
-
-            IsPlaying = false;
-            IsComplete = false;
-            IsPausing = false;
-        }
-
-        public void Restart()
+        public override void Restart()
         {
             _durationValue = 0f;
-            Play();
-        }
-
-        public void Pause()
-        {
-            MonoMultiThread.PauseThreadWorker(_threadKey);
-            IsPlaying = false;
-            IsPausing = true;
-            _stopwatch.Stop();
+            base.Restart();
         }
 
         public bool IsActive()
@@ -152,11 +114,11 @@ namespace Game.Tweener.Core
 
         private object WorkThreadAction(object _)
         {
-            var easeValue = Utility.Curves.ExecuteEaseFunc(_ease, Mathf.Min(_durationValue / _duration, 1));
-            _durationValue += _stopwatch.ElapsedTicks / 10000000f;
-            _stopwatch.Restart();
+            var easeValue = Utility.Curves.ExecuteEaseFunc(Ease, Mathf.Min(_durationValue / Duration, 1));
+            _durationValue += Stopwatch.ElapsedTicks / 10000000f;
+            Stopwatch.Restart();
 
-            return _tweenFunc.Evaluate(_startValue, _endValue, _from, easeValue);
+            return _tweenFunc.Evaluate(_startValue, _endValue, base.From, easeValue);
         }
 
         private bool MainThreadAction(object obj)
@@ -166,21 +128,21 @@ namespace Game.Tweener.Core
             _setter(value);
             OnUpdate?.Invoke();
 
-            if (!(_durationValue > _duration))
+            if (!(_durationValue > Duration))
             {
                 return false;
             }
             
             OnComplete?.Invoke();
-            IsComplete = !_loop;
+            IsComplete = !Loop;
 
-            if (!_loop)
+            if (!Loop)
             {
                 MonoMultiThread.Instance.StartCoroutine(Disable(this));
                 return true;
             }
 
-            switch (_loopType)
+            switch (LoopType)
             {
                 case LoopType.Normal:
                     break;
@@ -190,7 +152,7 @@ namespace Game.Tweener.Core
                     _endValue = tmp;
                     break;
                 case LoopType.Continue:
-                    if (_from)
+                    if (base.From)
                     {
                         _startValue = _getter();
                     }
