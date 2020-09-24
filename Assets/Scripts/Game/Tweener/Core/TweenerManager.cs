96bf9fa59a09ded89f1ba7e71afd89fb3c528869
HULKSTD <hulk1257@gmail.com>
2020-09-24 10:39:59 +0900

    Rename TweenerObjectPooling

--- Assets/Scripts/Game/Tweener/Core/TweenerManager.cs
+++ Assets/Scripts/Game/Tweener/Core/TweenerManager.cs
@@ -6,12 +6,12 @@ namespace Game.Tweener.Core
     public static class TweenerManager<T1, TTweenData>
         where TTweenData : ITweenData<T1>
     {
-        private static readonly Utility.ObjectPooling<Tweener<T1, TTweenData>> ObjectPooling =
+        private static readonly Utility.ObjectPooling<Tweener<T1, TTweenData>> TweenerObjectPooling =
             new Utility.ObjectPooling<Tweener<T1, TTweenData>>();
 
         public static Tweener<T1, TTweenData> GetTweener()
         {
-            return ObjectPooling.PopObject();
+            return TweenerObjectPooling.PopObject();
         }
     }
 }
